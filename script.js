f33c68ae3e270dbb43afaa3f892c21ba634b3cd6
savankan <kansagrasavan108@gmail.com>
2022-02-26 17:47:57 +0530

    created backpack object

---
+++ script.js
@@ -0,0 +1,14 @@
+import Backpack from "./Backpack.js";
+
+const everydayPack = new Backpack(
+    "everyday backpack",
+    30,
+    "gray",
+    15,
+    26,
+    26,
+    false
+    );
+
+console.log("everyday backpack object:",everydayPack);
+console.log("the pocketNum value:", everydayPack.pocketNum);
